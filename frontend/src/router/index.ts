b414e4765589ad4e3811585b5c4adae14faded32
st188130 <st188130@stud.uni-stuttgart.de>
2025-01-08 22:20:07 +0100

    deleted unnecessary class for creating a todo which is now implemented in ToDoView.vue because of the template

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -4,7 +4,6 @@ import AboutView from '@/views/AboutView.vue';
 import AssigneesView from '@/views/assignees/AssigneesView.vue';
 import ToDoView from '@/views/todos/ToDoView.vue';
 import CreateEditAssignee from '@/views/assignees/CreateEditAssignee.vue';
-import CreateToDo from "@/views/todos/CreateToDo.vue";
 import EditToDo from "@/views/todos/EditToDo.vue";
 import AssigneeByIdView from '@/views/assignees/AssigneeByIdView.vue';
 import ToDoByIdView from '@/views/todos/ToDoByIdView.vue';
@@ -17,7 +16,6 @@ const routes = [
   { path: '/create-assignee', component: CreateEditAssignee },
   { path: '/assignees/:id/edit', component: CreateEditAssignee },
   { path: '/todos', component: ToDoView },
-  { path: '/create-todo', component: CreateToDo },
   { path: '/todos/:id/edit', component: EditToDo },
   { path: '/assignee-by-id', component: AssigneeByIdView },
   { path: '/todo-by-id', component: ToDoByIdView },
